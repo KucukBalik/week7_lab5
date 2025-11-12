ac4fc9e87c262809e19c60b85b147ba26c3b77d8
DERVISEMREDEMIR-STUD <g00427391@atu.ie>
2025-11-12 12:09:41 +0000

    first commit

---
+++ src/main/java/ie/atu/cicdlab5/CicdLab5Application.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdlab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdLab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdLab5Application.class, args);
+    }
+
+}
