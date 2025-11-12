da37db89759d4eabbe182f915219ffdb7e66f0bf
DERVISEMREDEMIR-STUD <g00427391@atu.ie>
2025-11-12 13:42:17 +0000

    Delete and update operations are added. Persons can be updated or deleted by their id.

--- src/main/java/ie/atu/cicdlab5/PersonRepository.java
+++ src/main/java/ie/atu/cicdlab5/PersonRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findById(Long id);
+
     Optional<Person> findByEmployeeId(String employeeId);
 
     Person removePersonById(Long id);
